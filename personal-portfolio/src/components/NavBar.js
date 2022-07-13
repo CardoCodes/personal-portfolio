9ea3951f4351ced3c9b18b221ae358c54ad8ab1f
Ricardo Mendez <rmendez13@miners.utep.edu>
2022-07-13 04:23:58 +0000

    nav icons added

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar, Container, NavbarBrand, Nav } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 export const Navbar = () =>{
+
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+          if (window.scrollY > 50) {setScrolled(true);} else {setScrolled(false);}
+        }
+        window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);}, [])
+
+
    <Navbar expand="lg">
         <Container> 
             <NavbarBrand href="#home">
@@ -18,6 +35,15 @@ export const Navbar = () =>{
                     <Nav.Link href="#skills">Skills</Nav.Link>
                     <Nav.Link href="#projects">Project</Nav.Link>
                 </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+                        <a href="#"><img src={navIcon1} alt=""/></a>
+                        <a href="#"><img src={navIcon2} alt=""/></a>
+                        <a href="#"><img src={navIcon3} alt=""/></a>
+                    </div>
+                    <button className="test" onClick={() => console.log('connect')}><span>Let's Connect</span>
+                </span>
+
             </NavbarCollapse>
         </Container>
    </Navbar>
